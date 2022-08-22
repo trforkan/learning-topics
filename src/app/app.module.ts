ae1cc2537032f494522d1ee2ac48f10d6eea8551
Touhidur Rahman <trforkan@gmail.com>
2022-08-22 15:03:00 +0600

    22th August

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
   ],
   imports: [
     BrowserModule,
