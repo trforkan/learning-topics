c07176383d0e0834d627a5decd9e146541ceb4c2
Touhidur Rahman <trforkan@gmail.com>
2022-08-23 13:54:54 +0600

    23rd August

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,34 +1,104 @@
 import { customData } from './../../model/models';
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
-export class RecipeListComponent implements OnInit, AfterViewInit {
-
-  @Input('customDataAlias') public customData: customData[]  = [];
+export class RecipeListComponent
+  implements
+    OnInit,
+    AfterViewInit,
+    AfterViewChecked,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    OnDestroy
+{
+  @Input('customDataAlias') public customData: customData[] = [];
 
   @Output() public outputValue = new EventEmitter<number>();
 
   @ViewChild('nameRef', { static: true }) nameElementRef!: ElementRef;
 
+  loginInfo: { name: string; password: string }[] = [];
+
+  loginInput: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor() {
+    console.log('Constructor is called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit is called');
+  }
 
   ngAfterViewInit() {
     // this.nameElementRef?.nativeElement.focus();
-    console.log("viewChild = ",this.nameElementRef.nativeElement.value);
+    console.log('viewChild = ', this.nameElementRef.nativeElement.value);
   }
 
-  constructor() { }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges happened');
+    console.log(changes);
+  }
 
-  ngOnInit(): void {
-    console.log(this.customData);
+  ngDoCheck() {
+    console.log('ngDoCheck is called');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit is Called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked is called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChanged is called');
+  }
+
+  ngOnDestroy(): void {
+    console.log("nghOnDestroy is called");
   }
 
   deleteItem(value: number = 10) {
+    // this.loginInfo.splice(1,1);
+    this.customData.splice(0,1);
+    console.log(this.customData);
     this.outputValue.emit(value);
     // console.log(this.outputValue);
   }
 
+  dataInsert() {
+    this.loginInfo.push(this.loginInput.value);
+    console.log(this.loginInfo);
+  }
+
+  changekor() {
+    this.customData[0].age = 50;
+  }
 }
