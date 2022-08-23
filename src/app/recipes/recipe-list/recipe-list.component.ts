6e48a163ee3ce7958b834835b126f05b6cc89db9
Touhidur Rahman <trforkan@gmail.com>
2022-08-23 12:00:30 +0600

    23rd August

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,24 @@
 import { customData } from './../../model/models';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, AfterViewInit {
 
   @Input('customDataAlias') public customData: customData[]  = [];
 
   @Output() public outputValue = new EventEmitter<number>();
+
+  @ViewChild('nameRef', { static: true }) nameElementRef!: ElementRef;
+
+
+  ngAfterViewInit() {
+    // this.nameElementRef?.nativeElement.focus();
+    console.log("viewChild = ",this.nameElementRef.nativeElement.value);
+  }
 
   constructor() { }
 
