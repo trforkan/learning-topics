fe71ca466b73f349d8e7756c5887a6463bb33b97
Touhidur Rahman <trforkan@gmail.com>
2022-08-29 13:34:16 +0600

    29th August

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-udemy';
   number = 10;
+
+
+  ngOnInit() {
+    // this.processOrder("forkan");
+
+    this.takeOrder('Forkan', (customer:any)=>{
+      this.processOrder(customer, (customer:any)=>{
+        this.completeOrder(customer);
+      });
+    });
+  }
+
+
+  takeOrder = (customer: any, callback:any) => {
+    console.log(`taking order for ${customer}`);
+    callback(customer);
+  };
+
+  completeOrder = (customer: any) => {
+    console.log(`order completed for ${customer}`);
+  };
+
+
+  processOrder = (customer:any, callback:any) => {
+    console.log(`processing order for ${customer}`);
+
+    setTimeout(() => {
+      console.log("cooking complete");
+      console.log(`order processed for ${customer}`);
+      callback(customer);
+    },3000);
+
+    // callback()
+
+  };
+
+
+
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
@@ -12,10 +13,87 @@ export class ServerComponent implements OnInit {
   bgColor = 'white';
   txtColor = 'black';
 
+
+  ara = [
+    {
+      name: "forkan",
+      email: "trforkan@gmail.com",
+      gender :"male",
+      age: 26
+    },
+    {
+      name: "Prianka",
+      email: "priya@gmail.com",
+      gender: "female",
+      age: 24
+    },
+    {
+      name: "PriyaRani",
+      email: "priyarani@gmail.com",
+      gender: "female",
+      age: 24
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+
+    console.log(this.ara);
+
+
+    const flag_Every = this.ara.every( p => p.age > 24);  //every
+    console.log("flag_Every = ",flag_Every);
+
+    const initial_ara = {
+        name: "",
+        email: "",
+        gender: "",
+        age: 0
+      };
+
+
+    const mapping = this.ara.map(p => p.name).join('|');
+    console.log("map = ",mapping);
+
+
+    const ara_filter = this.ara.filter( p => p.age <= 25 );  // filter
+    console.log("filter = ", ara_filter);
+
+    const ara_find = this.ara.find(p => p.age<=24);  // find (retuns the first find data from array that matches the condition)
+    console.log("find = ",ara_find);
+
+
+    const ara_index = this.ara.findIndex(p => p.age<=26); //find index
+    console.log("find_index = ",ara_index);
+
+
+    const poped = this.ara.pop();
+    console.log("pop = " , poped);
+
+
+    const ara_filled = this.ara.fill(initial_ara,1,3);  //fill
+    console.log("fill = ",ara_filled);
+
+      const promise = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("Primise Working");
+        },1000)
+      })
+
+      // promise.then(res => console.log(res));
+      console.log(promise);
+
+      const  observable = new Observable(res => {
+        setTimeout(() => {
+          res.next('Observable  working')
+        },1000)
+      });
+      // observable.subscribe(res => console.log(res));
+      console.log(observable)
+
   }
+
 
 
   display(event: Event) {
